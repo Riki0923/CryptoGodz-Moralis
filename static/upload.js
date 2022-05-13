de5d05e12e6e49ff34f323a11282cf413275dbbf
Qudusayo <qqudusayo@gmail.com>
2022-05-13 08:00:27 +0100

    Fix

--- static/upload.js
+++ static/upload.js
@@ -1,19 +1,13 @@
-Moralis.initialize("ElUr7RZ9LgJNDvnDi02ajnrO49wS4vqi43wEdrPr"); // Application id from moralis.io
-Moralis.serverURL = "https://qxsyq5mbz9gk.usemoralis.com:2053/server"; //Server url from moralis.io
-
-//Moralis.authenticate()
-
-async function upload(){
+async function upload() {
     const fileInput = document.getElementById("files");
-    const ipfsUris = {}
-    for (i=0;i< fileInput.files.length; i++){
+    const ipfsUris = {};
+    for (i = 0; i < fileInput.files.length; i++) {
         console.log(fileInput.files[i].name);
         let data = fileInput.files[i];
         let imageFile = new Moralis.File(data.name, data);
         await imageFile.saveIPFS();
         console.log(imageFile.ipfs(), imageFile.hash());
-        ipfsUris[i+1] = imageFile.ipfs();
+        ipfsUris[i + 1] = imageFile.ipfs();
     }
     console.log(ipfsUris);
-
 }
